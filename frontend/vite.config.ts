78a37d236b8fda1ce58df2380780b17683564530
vinhlock05 <tonvinhloc1018@gmail.com>
2025-08-21 15:11:49 +0700

    fix deploy vercel

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   css: {
@@ -14,5 +13,10 @@ export default defineConfig({
     hmr: {
       port: 5173,
     },
+  },
+  base: '/',
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true,
   },
 })
